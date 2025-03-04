b477945f7a9a66fac3a7501cbbbcc8e09319e0a2
elciclon <33224715+elciclon@users.noreply.github.com>
2025-03-04 16:49:55 -0300

    Start implementing classes

--- kodland_game.py
+++ kodland_game.py
@@ -7,6 +7,12 @@ from pygame import Rect
 WIDTH = 800
 HEIGHT = 600
 
+# Global variables
+state = "menu"
+music = True
+score = 0
+game_over = False
+
 # Buttons class
 class Button:
     def __init__(self, x, y, width, height, text):
@@ -33,12 +39,17 @@ class Hero:
         self.y = HEIGHT // 2
         self.speed = 3
         # hero images
-        self.idle_images = ['character_maleperson_idle', 'character_malePerson_duck']
+        self.idle_images = ['hero_1', 'hero_2']
 
+        self.current_images = self.idle_images
+        self.frame = 0
+        self.actor = Actor(self.current_images[self.frame], center=(self.x, self.y))
+        self.animation_timer = 0
 
-hero = Actor('character_maleperson_idle')
-hero.x = WIDTH // 2
-hero.y = HEIGHT // 2
+    def draw(self):
+        self.actor.draw()
+
+hero = Hero()
 
 zombie = Actor('character_zombie_idle')
 zombie.x = random.randint(20, WIDTH - 20)
@@ -49,29 +60,37 @@ items.x = random.randint(20, WIDTH - 20)
 items.y = random.randint(20, HEIGHT - 20)
 
 def update():
+    global score, game_over, music, state
+
     if keyboard.left:
         hero.x = hero.x - 5
     if keyboard.right:
         hero.x = hero.x + 5
 
-    zombie.y = zombie.y + 4
+    zombie.y = zombie.y + 4 + score / 10
     if zombie.y > HEIGHT:
         zombie.y = 0
-    if zombie.colliderect(hero):
-        zombie.x = random.randint(20, WIDTH - 20)
-        zombie.y = 0
-
-    if hero.colliderect(items):
-        items.x = random.randint(20, WIDTH - 20)
-        items.y = random.randint(20, HEIGHT - 20)
+    
+    # if hero.colliderect(items):
+    #     items.x = random.randint(20, WIDTH - 20)
+    #     items.y = random.randint(20, HEIGHT - 20)
+    #     score = score + 1
 
 
 
 def draw():
     screen.clear()
-    zombie.draw()
-    hero.draw()
-    items.draw()
+    if game_over:
+        screen.draw.text('Game Over', (WIDTH // 2, 300), color=(192,31,31), fontsize=50)
+        screen.draw.text('Score: ' + str(score), (WIDTH // 2, 350), color=(255,255,255), fontsize=60)
+    else:
+        zombie.draw()
+        hero.draw()
+        items.draw()
+        screen.draw.text('Score: ' + str(score), (15,10), color=(255,255,255), fontsize=30)
+        start_game_button.draw()
+        music_button.draw()
+        exit_button.draw()
 
 
 pgzrun.go()
